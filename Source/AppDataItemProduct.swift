bb8c4c6d9fc16aebf9a68eeee162c7af685fa35f
206549143 <a206549143@NBCUAdmins-MacBook-Pro.local>
2017-10-12 17:22:33 -0700

    testing

--- Source/AppDataItemProduct.swift
+++ Source/AppDataItemProduct.swift
@@ -96,70 +96,72 @@ open class AppDataItemProduct: AppDataItem, ProductItem {
         var sceneImageBullseyeX: Double?
         var sceneImageBullseyeY: Double?
 
-        for indexer in indexer[Elements.NVPair] {
-            // Name
-            guard let name: String = indexer.value(ofAttribute: Attributes.Name) else {
-                throw ManifestError.missingRequiredAttribute(Attributes.Name, element: indexer.element)
-            }
-
-            switch name {
-            case AppDataNVPairName.ExternalURL:
-                // URL
-                guard let externalURLString: String = try indexer[Elements.URL].value(), let externalURL = URL(string: externalURLString) else {
-                    throw ManifestError.missingRequiredChildElement(name: Elements.URL, element: indexer.element)
+        if let elementsArray = indexer[Elements.NVPair].array {
+            for indexer in elementsArray {
+                // Name
+                guard let name: String = indexer.value(ofAttribute: Attributes.Name) else {
+                    throw ManifestError.missingRequiredAttribute(Attributes.Name, element: indexer.element)
                 }
 
-                self.externalURL = externalURL
-                break
+                switch name {
+                case AppDataNVPairName.ExternalURL:
+                    // URL
+                    guard let externalURLString: String = try indexer[Elements.URL].value(), let externalURL = URL(string: externalURLString) else {
+                        throw ManifestError.missingRequiredChildElement(name: Elements.URL, element: indexer.element)
+                    }
 
-            case AppDataNVPairName.Price:
-                // Money
-                guard let price: Double = try indexer[Elements.Money].value() else {
-                    throw ManifestError.missingRequiredChildElement(name: Elements.Money, element: indexer.element)
+                    self.externalURL = externalURL
+                    break
+
+                case AppDataNVPairName.Price:
+                    // Money
+                    guard let price: Double = try indexer[Elements.Money].value() else {
+                        throw ManifestError.missingRequiredChildElement(name: Elements.Money, element: indexer.element)
+                    }
+
+                    self.price = price
+                    currencyCode = (indexer[Elements.Money].value(ofAttribute: Attributes.Currency) ?? "USD")
+                    break
+
+                case AppDataNVPairName.ExactMatch:
+                    isExactMatch = try indexer[Elements.Text].value()
+                    hasExactMatchData = true
+                    break
+
+                case AppDataNVPairName.SceneImage:
+                    sceneImagePictureID = try indexer[Elements.PictureID].value()
+                    break
+
+                case AppDataNVPairName.ProductVideo:
+                    productVideoPresentationID = try indexer[Elements.PresentationID].value()
+                    break
+
+                case AppDataNVPairName.ProductVideoContentID:
+                    productVideoContentID = try indexer[Elements.ContentID].value()
+                    break
+
+                case AppDataNVPairName.ProductImageBullseyeX:
+                    productImageBullseyeX = try indexer[Elements.Decimal].value()
+                    break
+
+                case AppDataNVPairName.ProductImageBullseyeY:
+                    productImageBullseyeY = try indexer[Elements.Decimal].value()
+                    break
+
+                case AppDataNVPairName.SceneImageBullseyeX:
+                    sceneImageBullseyeX = try indexer[Elements.Decimal].value()
+                    break
+
+                case AppDataNVPairName.SceneImageBullseyeY:
+                    sceneImageBullseyeY = try indexer[Elements.Decimal].value()
+                    break
+
+                default:
+                    break
                 }
-
-                self.price = price
-                currencyCode = (indexer[Elements.Money].value(ofAttribute: Attributes.Currency) ?? "USD")
-                break
-
-            case AppDataNVPairName.ExactMatch:
-                isExactMatch = try indexer[Elements.Text].value()
-                hasExactMatchData = true
-                break
-
-            case AppDataNVPairName.SceneImage:
-                sceneImagePictureID = try indexer[Elements.PictureID].value()
-                break
-
-            case AppDataNVPairName.ProductVideo:
-                productVideoPresentationID = try indexer[Elements.PresentationID].value()
-                break
-
-            case AppDataNVPairName.ProductVideoContentID:
-                productVideoContentID = try indexer[Elements.ContentID].value()
-                break
-
-            case AppDataNVPairName.ProductImageBullseyeX:
-                productImageBullseyeX = try indexer[Elements.Decimal].value()
-                break
-
-            case AppDataNVPairName.ProductImageBullseyeY:
-                productImageBullseyeY = try indexer[Elements.Decimal].value()
-                break
-
-            case AppDataNVPairName.SceneImageBullseyeX:
-                sceneImageBullseyeX = try indexer[Elements.Decimal].value()
-                break
-
-            case AppDataNVPairName.SceneImageBullseyeY:
-                sceneImageBullseyeY = try indexer[Elements.Decimal].value()
-                break
-
-            default:
-                break
             }
         }
-
+            
         if sceneImagePictureID != nil, let x = sceneImageBullseyeX, let y = sceneImageBullseyeY {
             bullseyePoint = CGPoint(x: x, y: y)
         } else if let x = productImageBullseyeX, let y = productImageBullseyeY {
